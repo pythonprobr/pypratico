9a8b046ab294093fcfecd17e68d4512a532d093c
Luciano Ramalho <luciano@ramalho.org>
2013-12-09 19:44:19 -0200

    relogio com classe

---
+++ gui/tk/relogio.py
@@ -0,0 +1,29 @@
+"""
+O mais breve programa Tkinter que faz alguma coisa, escrito da forma
+mais breve que eu consegui, mas necessariamente a melhor.
+"""
+
+import tkinter
+from time import strftime
+
+class Janela(tkinter.Frame):
+
+    def __init__(self, pai):
+        super().__init__(pai)
+        self.visor = tkinter.Label(self)
+        self.visor['font'] = 'Helvetica 120 bold'
+        self.pack()
+        self.visor.pack()
+        self.tac()
+
+    def tic(self):
+        self.visor['text'] = strftime('%H:%M:%S')
+
+    def tac(self):
+        self.tic()
+        self.after(1000, self.tac)
+
+
+root = tkinter.Tk()
+janela = Janela(root)
+root.mainloop()  # inicia o loop de eventos do Tkinter
