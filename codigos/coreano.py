18d84418b378cb2dce8ad01c0a248ad8ed18806c
Luciano Ramalho <luciano@ramalho.org>
2013-12-09 14:32:15 -0200

    exemplos unicode

---
+++ codigos/coreano.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+"""Exemplo de verso em coreano:
+
+그동안 무척이나 잊어먹고 있던 것이 죽음이었구나
+
+"The thing I have been forgetting about until now was death"
+
+Autor: Ko Un
+http://en.wikipedia.org/wiki/Ko_Un
+
+Fonte:
+https://twitter.com/Goeun_bot/status/405607418748354560
+
+Tradução de:
+James Lee
+"""
+
+verso = "그동안 무척이나 잊어먹고 있던 것이 죽음이었구나"
+
+print('%d caracteres' % len(verso))
+print(verso)
+
+for letra in verso[:3]:
+    print(letra)
