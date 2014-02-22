2256dc06311d032b184ac4d2d54b992c9eaf5ad3
Luciano Ramalho <luciano@ramalho.org>
2014-02-22 20:37:32 -0300

    exemplos cars_terminal testados no Windows 8.1 (codepage 850)

--- codigos/cars_terminal.py
+++ codigos/cars_terminal.py
@@ -18,13 +18,12 @@ cod_saida = sys.stdout.encoding
 
 if cod_saida.upper().startswith('UTF'):
     print('Terminal %s, exibindo caracteres Unicode U+0020 a U+00FF' % cod_saida)
-    cars_uni = [chr(i) for i in range(0x20, 0xff)]
+    cars_uni = [chr(i) for i in range(0x20, 0x100)]
 else:
     print('Terminal %s, exibindo caracteres 0x20 a 0xff' % cod_saida)
-    cars_uni = [int2byte(i).decode(cod_saida) for i in range(0x20, 0xff)]
+    cars_uni = [int2byte(i).decode(cod_saida) for i in range(0x20, 0x100)]
 
-for car in cars_uni:
+for i, car in enumerate(cars_uni):
     print(car, end=' ')
-    if ord(car) % 32 == 31:
+    if i % 32 == 31:
         print()
-print()
--- codigos/cars_terminal_py3.py
+++ codigos/cars_terminal_py3.py
@@ -5,11 +5,15 @@ Exibe os caracteres de codigo 32 a 255 suportados pelo terminal
 import sys
 
 cod_saida = sys.stdout.encoding
-print('Encoding:', cod_saida)
 
-for i in range(32, 256):
-    octeto = bytes([i])
-    car_uni = octeto.decode(cod_saida)
-    print(car_uni, end=' ')
+if cod_saida.upper().startswith('UTF'):
+    print('Terminal %s, exibindo caracteres Unicode U+0020 a U+00FF' % cod_saida)
+    cars_uni = [chr(i) for i in range(0x20, 0x100)]
+else:
+    print('Terminal %s, exibindo caracteres 0x20 a 0xff' % cod_saida)
+    cars_uni = bytes(range(0x20, 0x100)).decode(cod_saida)
+
+for i, car in enumerate(cars_uni):
+    print(car, end=' ')
     if i % 32 == 31:
         print()
