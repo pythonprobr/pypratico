1bd12c1e18bdfbbc2a0c93f69679a2744628fa28
Luciano Ramalho <luciano@ramalho.org>
2014-02-22 20:42:19 -0300

    cars_terminal compativel com utf8

--- codigos/cars_terminal.py
+++ codigos/cars_terminal.py
@@ -18,7 +18,7 @@ cod_saida = sys.stdout.encoding
 
 if cod_saida.upper().startswith('UTF'):
     print('Terminal %s, exibindo caracteres Unicode U+0020 a U+00FF' % cod_saida)
-    cars_uni = [chr(i) for i in range(0x20, 0x100)]
+    cars_uni = [unichr(i) for i in range(0x20, 0x100)]
 else:
     print('Terminal %s, exibindo caracteres 0x20 a 0xff' % cod_saida)
     cars_uni = [int2byte(i).decode(cod_saida) for i in range(0x20, 0x100)]
